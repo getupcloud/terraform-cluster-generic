d770cd3da96f415a3db50898c2b59a4b9b183b8b
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-30 00:36:46 -0300

    wip

--- generic_modules.tf
+++ generic_modules.tf
@@ -8,7 +8,7 @@ resource "local_file" "debug-modules" {
 
 module "cert-manager" {
   count  = local.modules.cert-manager.enabled ? 1 : 0
-  source = "github.com/getupcloud/terraform-module-cert-manager?ref=v2.0.0-alpha2"
+  source = "github.com/getupcloud/terraform-module-cert-manager?ref=v2.0.0-alpha3"
 
   cluster_name  = var.cluster_name
   customer_name = var.customer_name
