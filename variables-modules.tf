be2f233c469543f693b4c61c5d7a060dc6ad19d4
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-12-09 16:39:29 -0300

    remove modules moved to topleve

--- variables-modules.tf
+++ variables-modules.tf
@@ -3,45 +3,11 @@
 
 variable "modules_defaults" {
   description = "Configure modules to install (defaults)"
-  type = object({
-    cert-manager = object({
-      enabled         = bool
-      hosted_zone_ids = list(string)
-    })
-    external-dns = object({
-      enabled         = bool
-      private         = bool
-      hosted_zone_ids = list(string)
-      domain_filters  = list(string)
-    })
-    velero = object({
-      enabled     = bool
-      bucket_name = string
-    })
-  })
+  type        = object({})
 
-  default = {
-    cert-manager = {
-      enabled         = false
-      hosted_zone_ids = []
-    }
-    external-dns = {
-      enabled         = false
-      private         = false
-      hosted_zone_ids = []
-      domain_filters  = []
-    }
-    velero = {
-      enabled     = false
-      bucket_name = ""
-    }
-  }
+  default = {}
 }
 
 locals {
-  register_modules = {
-    cert-manager : local.modules.cert-manager.enabled ? module.cert-manager[0] : {}
-    # external-dns : local.modules.external-dns.enabled ? module.external-dns[0] : {}
-    velero : local.modules.velero.enabled ? module.velero[0] : {}
-  }
+  register_modules = {}
 }
