970e247241c8c3d129379c3374ca3c87740221a0
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-12-09 13:15:05 -0300

    Build local.modules from script ./make-modules

--- variables-modules.tf
+++ variables-modules.tf
@@ -39,23 +39,6 @@ variable "modules_defaults" {
 }
 
 locals {
-  modules = merge(var.modules_defaults, var.modules, {
-    cert-manager = {
-      enabled         = try(var.modules.cert-manager.enabled, var.modules_defaults.cert-manager.enabled)
-      hosted_zone_ids = try(var.modules.cert-manager.hosted_zone_ids, var.modules_defaults.cert-manager.hosted_zone_ids)
-    }
-    external-dns = {
-      enabled         = try(var.modules.external-dns.enabled, var.modules_defaults.external-dns.enabled)
-      private         = try(var.modules.external-dns.private, var.modules_defaults.external-dns.private)
-      hosted_zone_ids = try(var.modules.external-dns.hosted_zone_ids, var.modules_defaults.external-dns.hosted_zone_ids)
-      domain_filters  = try(var.modules.external-dns.domain_filters, var.modules_defaults.external-dns.domain_filters)
-    }
-    velero = {
-      enabled     = try(var.modules.velero.enabled, var.modules_defaults.cert-manager.enabled)
-      bucket_name = try(var.modules.velero.bucket_name, var.modules_defaults.velero.bucket_name)
-    }
-  })
-
   register_modules = {
     cert-manager : local.modules.cert-manager.enabled ? module.cert-manager[0] : {}
     # external-dns : local.modules.external-dns.enabled ? module.external-dns[0] : {}
