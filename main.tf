d446e3331bf95e080e75c87f4791306408953377
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-08-08 17:32:00 +0100

    flux bump

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "teleport-agent" {
 }
 
 module "flux" {
-  source = "github.com/getupcloud/terraform-module-flux?ref=v2.3.0"
+  source = "github.com/getupcloud/terraform-module-flux?ref=v2.4.0"
 
   git_repo                = var.flux_git_repo
   manifests_path          = "./clusters/${var.cluster_name}/${var.cluster_type}/manifests"
